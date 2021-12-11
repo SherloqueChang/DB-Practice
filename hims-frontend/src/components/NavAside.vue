9b6d86a4949795e72ecdc249cd8c9b80099794a1
SherloqueChang <1410945936@qq.com>
2021-12-11 14:52:10 +0800

    patient

--- hims-frontend/src/components/NavAside.vue
+++ hims-frontend/src/components/NavAside.vue
@@ -3,20 +3,24 @@
     <el-aside>
       <el-menu class="el-menu-vertical-demo" router>
         <el-menu-item index="/userInfo" v-if="!isAdmin">
-          <i class="el-icon-user"></i>个人信息
+          <i class="el-icon-user"></i><span slot="title">个人信息</span>
         </el-menu-item>
-        <el-menu-item index="/appointmentInfo" v-if="isPatient">
-          <i class="el-icon-info"></i>预约信息
-        </el-menu-item>
-        <el-menu-item index="/appointmentOnline" v-if="isPatient">
+        <el-submenu index="1" v-if="isAdmin || isPatient">
+          <template slot="title">
+            <i class="el-icon-info"></i><span>就医信息</span>
+          </template>
+          <el-menu-item index="/medicalInfo">就诊记录</el-menu-item>
+          <el-menu-item index="/appointmentInfo">预约记录</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="/appointmentOnline" v-if="isPatient || isAdmin">
           <i class="el-icon-circle-plus-outline"></i>线上预约
         </el-menu-item>
-        <el-menu-item index="/prevPatient" v-if="isDoctor">
+        <el-menu-item index="/prevPatient" v-if="isDoctor || isAdmin">
           <i class="el-icon-user-solid"></i>历史患者
         </el-menu-item>
-        <el-menu-item index="/appointmentToday" v-if="isDoctor">
+        <el-menu-item index="/appointmentToday" v-if="isDoctor || isAdmin">
           <i class="el-icon-edit-outline"></i>今日预约
-        </el-menu-item><el-menu-item index="/departmentMng" v-if="isLeader">
+        </el-menu-item><el-menu-item index="/departmentMng" v-if="isLeader || isAdmin">
           <i class="el-icon-s-home"></i>科室管理
         </el-menu-item><el-menu-item index="/medicineMng" v-if="isAdmin">
           <i class="el-icon-first-aid-kit"></i>药品管理
@@ -34,9 +38,9 @@ export default {
       user: {},
       id: '',
       password: '',
-      isPatient: true,
-      isDoctor: true,
-      isLeader: true,
+      isPatient: false,
+      isDoctor: false,
+      isLeader: false,
       isAdmin: false
     }
   },
--- hims-frontend/src/pages/AppointmentInfo.vue
+++ hims-frontend/src/pages/AppointmentInfo.vue
@@ -4,7 +4,29 @@
     <el-container>
       <el-aside><NavAside></NavAside></el-aside>
       <el-main>
-
+        <el-table
+          :data="tableData"
+          height="250"
+          border
+          empty-text="暂无预约记录"
+          style="width: 100%">
+          <el-table-column
+            prop="date"
+            label="日期">
+          </el-table-column>
+          <el-table-column
+            prop="department"
+            label="科室">
+          </el-table-column>
+          <el-table-column
+            prop="doctor"
+            label="医生">
+          </el-table-column>
+          <el-table-column
+            prop="status"
+            label="状态">
+          </el-table-column>
+        </el-table>
       </el-main>
     </el-container>
   </el-container>
@@ -20,7 +42,41 @@ export default {
   components: { NavHeader, NavAside },
   data () {
     return {
-      user: {}
+      user: {},
+      tableData: [],
+      laoding: false
+    }
+  },
+  created () {
+    this.handleUserData()
+    this.loadTableData()
+  },
+  methods: {
+    handleUserData () {
+      if (this.$store.state.user) {
+        this.user = this.$store.state.user
+      }
+    },
+    loadTableData () {
+      this.$axios
+        .get('/patientAppointmentInfo', {
+          params: { id: this.user.id }
+        })
+        .then((resp) => {
+          if (resp.status === 200) {
+            resp.data.patientAppointment.forEach((element) => {
+              this.tableData.push({
+                date: element.date,
+                department: element.department,
+                doctor: element.doctor,
+                status: element.status
+              })
+            })
+          }
+        })
+        .catch((error) => {
+          this.$message.error('请求错误，请重试')
+        })
     }
   }
 }
